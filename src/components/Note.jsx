491916b078ff7611e8c7e1a21ea0bd80cf5f9f15
gupta <p.gupta@cenit.com>
2021-01-03 03:01:42 +0100

    part 1 complete

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(){
+    return(
+        <div className = "note">
+            <h1>This is Notes title.</h1>
+            <p>This is notes content.</p>
+        </div>
+    )
+}
+
+export default Note;
